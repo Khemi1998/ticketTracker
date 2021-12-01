d7318a881f8018c9cc64c539ef96bbfba79cccbb
Khemi <khemi@Khemis-Laptop.local>
2021-12-01 21:02:03 +0000

    iteration9: counter works

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,19 +1,34 @@
 // Counter.jsx
-import React from "react";
+import React, { useState } from "react";
 import "./Counter.scss";
 
-
 const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleDecrement = () => {
+    if (counter !== 0) {
+      setCounter(counter - 1);
+    }
+  };
+
   return (
     <div className="EmployeeCard__counter">
       <h3>Counter</h3>
       <section className="counter__container">
-        <button className="counter__buttons__increment">
+        <button
+          className="counter__buttons__increment"
+          onClick={handleIncrement}
+        >
           <i className="fas fa-arrow-circle-up"></i>
         </button>
-        <h5 className="counter__amount">0</h5>
-        <button className="counter__buttons__decrement">
-          <i className="fas fa-arrow-circle-down"></i>
+        <h5 className="counter__amount">{counter}</h5>
+        <button
+          className="counter__buttons__decrement"
+          onClick={handleDecrement}><i className="fas fa-arrow-circle-down"></i>
         </button>
       </section>
     </div>
